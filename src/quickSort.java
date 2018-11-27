bc48d60d3ac82c9e96dbc158ead350eeef52eb5b
BT_1N3_30 <@>
2018-11-27 10:15:44 -0500

    asasfasf

--- src/quickSort.java
+++ src/quickSort.java
@@ -7,8 +7,8 @@ public class quickSort
     {
         this.left = left;
         this.right = right;
+        this.arr = arr;
     }
-
     public void quickSort(int[] arr, int left, int right)
     {
         if(left<right)
@@ -17,23 +17,25 @@ public class quickSort
             quickSort(arr,left,pivot-1);
             quickSort(arr,pivot+1,right);
         }
-        this.arr = arr;
+        toString();
     }
     public int partition(int[] arr, int left, int right)
     {
-        int pivot = right;
+        int pivot = arr[right];
         int i = left -1;
         for(int j = left; j<right; j++)
         {
             if(arr[j] <= pivot)
             {
-                i+=1;
-                int swap = arr[j];
+                i++;
+                int swap = arr[i];
                 arr[i] = arr[j];
                 arr[j] = swap;
             }
         }
-        arr[i+1] = arr[right];
+        int temp1 = arr[right];
+        arr[right] = arr[i+1];
+        arr[i+1] = temp1;
         return i+1;
     }
     public String toString()
@@ -41,7 +43,7 @@ public class quickSort
         String str = "";
         for(int j = 0; j<arr.length;j++)
         {
-            str+= arr[j];
+            str = " "+ arr[j];
         }
         return str;
     }
--- src/runnner.java
+++ src/runnner.java
@@ -1,9 +1,9 @@
 public class runnner
 {
-    public void main(String[] args)
+    public static void main(String[] args)
     {
         int[] test1 = {0,10,23,1024,123,12,312,3,123,12,4,45,46,3,7,3,4634,64,35,34534,};
         quickSort test2 = new quickSort(test1,0, 3);
-        System.out.println(test1);
+        System.out.println(test2);
     }
 }
